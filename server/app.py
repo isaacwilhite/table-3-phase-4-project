c41c37624b87521204a4d51e7608349b008eba43
Joseph <josephylee72@gmail.com>
2023-11-27 14:29:20 -0800

    relationships3

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ from flask_restful import Resource
 from config import app, db, api
 
 # Add your model imports
-from models import User #, Interest, Connection, Message, Event
+from models import User # Event
 
 # Views go here!
 
--- server/models.py
+++ server/models.py
@@ -7,6 +7,37 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 from datetime import datetime
 
+user_connections = db.Table(
+    'user_connections',
+    db.Column('user1_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user2_id', db.Integer, db.ForeignKey('users.id'))
+)
+
+user_messages = db.Table(
+    'user_messages',
+    db.Column('user1_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user2_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user1_messages', db.String),
+    db.Column('user2_messages', db.String)
+)
+
+user_events = db.Table(
+    'user_events',
+    db.Column('user1_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('user2_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('date', db.DateTime),
+    db.Column('type', db.String),
+    db.Column('location', db.String),
+    db.Column('details', db.String),
+    db.Column('status', db.String)
+)
+
+interests = db.Table(
+    'interests',
+    db.Column('type', db.DateTime),
+    db.Column('name', db.String),
+)
+
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
@@ -15,7 +46,7 @@ class User(db.Model, SerializerMixin):
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, onupdate=func.now())
     name = db.Column(db.String, nullable=False)
-    age = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     gender = db.Column(db.String, nullable=False)
@@ -25,51 +56,26 @@ class User(db.Model, SerializerMixin):
     location_range = db.Column(db.Integer)
     bio = db.Column(db.String)
     interests = db.Column(db.String)
+    swiped = db.Column(db.String)
+    rejected = db.Column(db.String)
 
-    sent_requests = db.relationship('Request', back_populates='sender_id')
-    received_requests = db.relationship('Request', back_populates='receiver_id')
+    connections = db.relationship('User',
+        secondary = user_connections,
+        primaryjoin = (user_connections.c.user1_id == id),
+        secondaryjoin = (user_connections.c.user2_id == id),
+        backref = 'connection'
+    )
 
-    sent_connections = db.relationship('Connection', back_populates='user1_id')
-    received_connections = db.relationship('Connection', back_populates='user2_id')
+    messages = db.relationship('User',
+        secondary = user_messages,
+        primaryjoin = (user_messages.c.user1_id == id),
+        secondaryjoin = (user_messages.c.user2_id == id),
+        backref = 'message'
+    )
 
-    sent_messages = db.relationship('Message', back_populates='user1_messages')
-    received_messages = db.relationship('Message', back_populates='user2_messages')
-
-    hosted_events = db.relationship('Event', 'user1_id')
-    guest_events = db.relationship('Event', 'user2_id')
-
-class Request(db.Model, SerializerMixin):
-    __tablename__ = 'requests'
-
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    status = db.Column(db.String)
-
-class Connection(db.Model, SerializerMixin):
-    __tablename__ = 'connections'
-
-    id = db.Column(db.Integer, primary_key=True)
-    user1 = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user2 = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-
-class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
-
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, server_default=func.now())
-
-    user1_messages = db.relationship('User', 'sent_messages')
-    user2_messages = db.relationship('User', 'received_messages')
-
-class Event(db.Model, SerializerMixin):
-    __tablename__ = 'events'
-
-    id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, server_default=func.now())
-    user1 = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user2 = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-    location = db.Column(db.String, nullable=False)
-    details = db.Column(db.String)
-    status = db.Column(db.String, nullable=False)
+    events = db.relationship('User',
+        secondary = user_events,
+        primaryjoin = (user_events.c.user1_id == id),
+        secondaryjoin = (user_events.c.user2_id == id),
+        backref = 'event'
+    )
