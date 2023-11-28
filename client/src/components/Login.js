75cf9be45036b1fbcf5fdd6f205a2f844a33c732
Joseph <josephylee72@gmail.com>
2023-11-28 13:03:25 -0800

    completed login and signup pages

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,14 @@
 const Login = () => {
   return (
-    <>
-      <h1>HELLO LOGIN</h1>
-    </>
+    <div className='modal'>
+      <h1 className='modaltitle'>Login</h1>
+      <h3 className='modaltag'>Please enter your email and password.</h3>
+      <input id='email' className='loginInput' type='text' placeholder="Enter Email"></input>
+      <input id='pass' className='loginInput' type='text' placeholder="Enter Password"></input>
+      <div>
+        <button className='modalbutton'>Login</button>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,15 @@
 const Signup = () => {
   return (
-    <>
-      <h1>HELLO SIGNUP</h1>
-    </>
+    <div className='modal'>
+      <h1 className='modaltitle'>Register New User</h1>
+      <h3 className='modaltag'>Please enter your email and password.</h3>
+      <input id='email' className='loginInput' type='text' placeholder="Enter Email"></input>
+      <input id='pass' className='loginInput' type='text' placeholder="Enter Password"></input>
+      <input id='confirmPass' className='loginInput' type='text' placeholder="Confirm Password"></input>
+      <div>
+        <button className='modalbutton'>Sign Up</button>
+      </div>
+    </div>
   )
 }
 
