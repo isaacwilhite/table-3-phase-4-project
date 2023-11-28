e727d8418378c0b2755456a7f42499a44418e0be
Joseph <josephylee72@gmail.com>
2023-11-27 19:32:13 -0800

    routes made

---
+++ client/src/components/Interests.js
@@ -0,0 +1,9 @@
+const Interests = () => {
+  return (
+    <>
+      <h1>HELLO INTERESTS</h1>
+    </>
+  )
+}
+
+export default Interests;
---
+++ client/src/components/Profile.js
@@ -0,0 +1,9 @@
+const Profile = () => {
+  return (
+    <>
+      <h1>HELLO PROFILE</h1>
+    </>
+  )
+}
+
+export default Profile;
