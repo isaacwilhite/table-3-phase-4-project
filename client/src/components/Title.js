e09fa4cb5cd6b787566e613fbd70f5e4d01b0399
Joseph <josephylee72@gmail.com>
2023-11-28 12:51:14 -0800

    created title page

--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -1,8 +1,13 @@
 const Title = () => {
   return (
-    <>
-      <h1>HELLO WORLD</h1>
-    </>
+    <div className='modal'>
+      <h1 className='modaltitle'>Welcome to Datesmith!</h1>
+      <h3 className='modaltag'>Please select an option:</h3>
+      <div>
+        <button className='modalbutton'>Login</button>
+        <button className='modalbutton'>Sign Up</button>
+      </div>
+    </div>
   )
 }
 
