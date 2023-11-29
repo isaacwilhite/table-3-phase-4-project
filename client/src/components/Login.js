756930500b87f9281817d39865852409d6d20837
Joseph <josephylee72@gmail.com>
2023-11-28 19:31:56 -0800

    more stuff

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
 
 const fetchUrl = 'http://127.0.0.1:5000'
 
