25be51e78c82927c3db700d24321d6c61eea1efe
Joseph Lee <130028511+joeylee08@users.noreply.github.com>
2023-11-30 21:24:39 -0800

    css tweaks

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,55 +1,57 @@
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Login = () => {
   const navigate = useNavigate()
   
-  const [email, setEmail] = useState('')
-  const [pass, setPass] = useState('')
+  // const [email, setEmail] = useState('')
+  // const [pass, setPass] = useState('')
 
-  const handleChange = (e) => {
-    if (e.target.id == 'email_input') {
-      setEmail(e.target.value)
-      console.log(email)
-    } else if (e.target.id == 'pass_input') {
-      setPass(e.target.value)
-      console.log(pass)
-    }
-  }
+  const formSchema = yup.object().shape({
+    email: yup.string().email('Invalid email').required("Please enter an email."),
+    password: yup.string().required("Please enter a password.").min(5)
+  })
 
-  const handleLogin = () => {
-    const data = {
-      "email": email,
-      "password": pass
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: formSchema,
+    onSubmit: (values) => {
+      fetch('/login', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values, null, 2),
+      })
+      .then((res) => {
+        if (res.status == 200) {
+          localStorage.setItem('user_active', 'true')
+          navigate('/userhome') 
+        } else {
+          alert('Invalid login data.')
+        }
+      })
     }
-    fetch(`/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then((res) => {
-      if (res.ok) {
-        localStorage.setItem('user_active', 'true')
-        navigate('/userhome')  
-      } else {
-        alert('Invalid user credentials.')
-      }
-    })
-  }
+  })
 
   return (
     <div className='modal'>
-      <h1 className='modaltitle'>Login</h1>
-      <h3 className='modaltag'>Please enter your email and password.</h3>
-      <input id='email_input' className='loginInput' type='text' onChange={handleChange}placeholder="Enter Email"></input>
-      <input id='pass_input' className='loginInput' type='password' onChange={handleChange}placeholder="Enter Password"></input>
-      <div>
-        <button className='modalbutton' onClick={handleLogin}>Login</button>
-        <button className='modalbutton' onClick={() => navigate('/')}>Cancel</button>
+      <form onSubmit={formik.handleSubmit} id='formikLogin'>
+        <h1 className='modaltitle'>Login</h1>
+        <h3 className='modaltag'>Please enter your email and password.</h3>
+        <input id='email' className='loginInput' type='text' onChange={formik.handleChange} value={formik.values.email} placeholder="Enter Email"></input>
+        <input id='password' className='loginInput' type='password' onChange={formik.handleChange} value={formik.values.password} placeholder="Enter Password"></input>
+        <div id='loginButtons'>
+          <button className='modalbutton' type='submit'>Login</button>
+          <button className='modalbutton' onClick={() => navigate('/')}>Cancel</button>
       </div>
-    </div>
+      </form>
+    </div>  
   )
 }
 
--- client/src/components/MeetUsers.js
+++ client/src/components/MeetUsers.js
@@ -1,39 +1,7 @@
-// import NavBar from './NavBar'
-// import Header from './Header'
-// import { useNavigate } from 'react-router-dom'
-// import { useEffect } from 'react'
-// import MeetUsersGM from './MeetUserGM'
-
-
-// const MeetUsers = () => {
-//   const navigate = useNavigate()
-  
-//   useEffect(() => {
-//     if (localStorage.getItem('user_active') == 'false') {
-//       navigate('/');
-//     }
-//   }, []);
-  
-//   const title = 'MEET USERS'
-//   return (
-//     <div className='container'>
-//       <Header title={title} />
-//       <NavBar />
-//       <div className='content'>
-//         <h1>Content goes here.</h1>
-//         <MeetUsersGM />
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default MeetUsers;
-
 import NavBar from './NavBar'
 import Header from './Header'
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
-import MeetUsersGM from './MeetUserGM'
 import UserCard from './UserCard'
 
 let index = 0
