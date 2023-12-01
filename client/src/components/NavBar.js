bdf21b3cc92d8a068fb0ec56d690ea6ff3908f90
Joseph Lee <130028511+joeylee08@users.noreply.github.com>
2023-11-30 18:07:30 -0800

    stuff

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,22 +4,12 @@ const NavBar = () => {
   return (
     <nav className='navbar'>
       <ul>
-<<<<<<< HEAD
         <li><NavLink to='/userhome'>Home</NavLink></li>
         <li><NavLink to='/profile'>Profile</NavLink></li>
         <li><NavLink to='/connections'>Connection</NavLink></li>
         <li><NavLink to='/meetusers'>Meet User</NavLink></li>
         <li><NavLink to='/createevent'>Create Event</NavLink></li>
         <li><NavLink to='/myevents'>My Events</NavLink></li>
-=======
-        <li><a href='/userhome'>Home</a></li>
-        <li><a href='/profile'>Profile</a></li>
-        <li><a href='/connections'>Connections</a></li>
-        <li><a href='/meetusers'>Meet Users</a></li>
-        <li><a href='/usersnearme'>Map of Users</a></li>
-        <li><a href='/createevent'>Create Event</a></li>
-        <li><a href='/myevents'>My Events</a></li>
->>>>>>> main
       </ul>
     </nav>
   )
