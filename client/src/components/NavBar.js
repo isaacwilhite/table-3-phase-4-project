9c4c989692ae76d21363083bbcc671fe096ba703
Joseph Lee <130028511+joeylee08@users.noreply.github.com>
2023-11-30 17:09:42 -0800

    matteo changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,13 +1,15 @@
+import { NavLink } from 'react-router-dom'
+
 const NavBar = () => {
   return (
     <nav className='navbar'>
       <ul>
-        <li><a href='/userhome'>Home</a></li>
-        <li><a href='/profile'>Profile</a></li>
-        <li><a href='/connections'>Connections</a></li>
-        <li><a href='/meetusers'>Meet Users</a></li>
-        <li><a href='/createevent'>Create Event</a></li>
-        <li><a href='/myevents'>My Events</a></li>
+        <li><NavLink to='/userhome'>Home</NavLink></li>
+        <li><NavLink to='/profile'>Profile</NavLink></li>
+        <li><NavLink to='/connections'>Connection</NavLink></li>
+        <li><NavLink to='/meetusers'>Meet User</NavLink></li>
+        <li><NavLink to='/createevent'>Create Event</NavLink></li>
+        <li><NavLink to='/myevents'>My Events</NavLink></li>
       </ul>
     </nav>
   )
